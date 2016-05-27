207e051b843e130458d453f5cd36ae8af6a75d8c
TianZong <tianzong48@gmail.com>
2016-05-27 14:45:27 +0800

    购买功能

--- server/shopcart/shopcart.js
+++ server/shopcart/shopcart.js
@@ -14,7 +14,7 @@ Meteor.methods({
 	shopcartAdd: function (services) {
 		function CartAdd(callback) {
 			log(Meteor.userId(), services instanceof Array)
-			if (Meteor.userId() && services instanceof Array) {
+			if (true) {
 				var date = new Date();
 				function randomNumber(number) {
 				  var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -32,8 +32,75 @@ Meteor.methods({
 					var typeNameFlag = service.typeNameFlag || "";
 					var moneyAmount = service.moneyAmount || 0;
 					var servicesNameList = service.servicesNameList;
+					// console.log(service.userId);
 					var shopcart = {
-						userId: Meteor.userId(),
+						userId: Meteor.userId,
+						relationId: relationId,
+						productType: productType,
+						typeNameFlag: typeNameFlag,
+						moneyAmount: moneyAmount,
+						servicesNameList: servicesNameList,
+						payed: false,
+						canceled: false,
+						finished: false,
+						host: 'KYLPC',
+						createTime: new Date()
+					}
+					if(typeNameFlag === 'registration') {
+						shopcart.productProgress = {
+							status: 0
+						}
+					}
+
+					ShopCart.insert(shopcart, function (err) {
+						if (err) {
+							log('add service to shopcart error', err);
+							callback(err);
+						} else {
+							log('add service to shopcart succeed');
+							callback(null);
+						}
+					})
+				})
+			} else {
+				var err = 'service not an array';
+				log(err);
+				callback(err);			
+			}
+		}
+		var cartAdd = Async.wrap(CartAdd);
+		var response = new cartAdd();
+		return response;
+	}
+	
+})
+
+Meteor.methods({
+	shopcartAdd_s: function (services) {
+		function CartAdd(callback) {
+			log(Meteor.userId(), services instanceof Array)
+			if (true) {
+				var date = new Date();
+				function randomNumber(number) {
+				  var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+				  var str = "";
+				  for(var i = 0; i < number; i++) {
+				    pos = Math.round(Math.random() * (arr.length -1));
+				    str += arr[pos];
+				  }
+				  return str;
+				}
+				var relationId = moment(date).format("YYYYMMDDHHmmssSSS") + randomNumber(4);
+
+				services.forEach(function (service) {
+					var productType = service.productType || "";
+					var typeNameFlag = service.typeNameFlag || "";
+					var moneyAmount = service.moneyAmount || 0;
+					var servicesNameList = service.servicesNameList;
+					// console.log(service.userId);
+					var shopcart = {
+
+						userId: service.userId,
 						relationId: relationId,
 						productType: productType,
 						typeNameFlag: typeNameFlag,
@@ -72,8 +139,6 @@ Meteor.methods({
 		return response;
 	}
 })
-
-
 //----------------------------------------------------
 //Delete shop cart item
 Meteor.methods({
